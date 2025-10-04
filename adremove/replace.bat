7b8fe58d9289c5ab96620be8ed17f902d730eb93
HackerAUG <augustinejameslinton@gmail.com>
2025-10-04 17:35:50 -0600

    replace.bat

--- adremove/replace.bat
+++ adremove/replace.bat
@@ -2,8 +2,9 @@
 setlocal
 for /r "." %%F in (details.html) do (
   if exist "%%~fF" (
-    copy /y "%~dp0adgone.html" "%%~fF" >nul
+    copy /y "%~dp0adbreak.html" "%%~fF" >nul
   )
 )
 
 endlocal
+
